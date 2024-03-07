22fc13af6b9926662ffedb3bb6959e38de0bd4d8
Jeison2001 <jeisondfuentes@unicesar.edu.co>
2024-03-07 18:42:03 -0500

    fix bug objetId

--- app.py
+++ app.py
@@ -1,53 +1,39 @@
+import json
 from fastapi import FastAPI
-from pydantic import BaseModel  
-from typing import Optional
 import uvicorn
 from pymongo import MongoClient
-from fastapi_utils.openapi import get_swagger_ui_html, get_openapi
+from fastapi.openapi.utils import get_openapi
+from bson import json_util
 
 app = FastAPI()
 
-client = MongoClient(
-    host="192.168.5.92",
-    port=27017,
-)
+client = MongoClient(host="192.168.5.92", port=27017)#172.17.0.2
+
 db = client["ejercicio_db"]
-collection = db["personal_informacion"]
+collection = db["personal_information"]
 
+@app.get("/documents")
+async def get_all_documents():
+    documents = collection.find({})
+    serialized_documents = json_util.dumps(documents)
+    json_legible = json.loads(serialized_documents)
+    return json_legible
 
-class User(BaseModel):
-    id: Optional[int]
-    tipoDocumento: str
-    documento: str
-    nombre1: str
-    nombre2: str
-    apellido1: str
-    apellido2: str
-    fechaNacimiento: str
-    sexo: str
-
-@app.get("/documents/", response_model=list[User])
-def get_all_documents():
-    documents = collection.find()
-    return documents
-
-
-@app.get("/documents/{tipo_documento}", response_model=User)
+@app.get("/documents/{tipo_documento}")
 def get_document_by_tipo(tipo_documento: str):
-    document = collection.find_one({"tipoDocumento": tipo_documento})
-    return document
-
+    document = collection.find({"tipoDocumento": tipo_documento})
+    serialized_document = json_util.dumps(document)
+    json_legible = json.loads(serialized_document)
+    return json_legible
 
 # Rutas para servir la documentación de Swagger
 @app.get("/docs", include_in_schema=False)
-async def get_swagger_ui_html():
-    return get_swagger_ui_html(openapi_url="/openapi.json")
-
+async def get_swagger_html():
+    return app.swagger_ui_html
 
 @app.get("/openapi.json", include_in_schema=False)
-async def get_openapi():
+async def get_openapi_endpoint():
     return get_openapi(app)
-
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000)
