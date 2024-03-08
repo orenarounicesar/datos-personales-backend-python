18eb372d14ccb359b6ba86543add0ae3ff9bc8d1
Fabian Ramirez <fabianramirez0072@gmail.com>
2024-03-07 19:47:12 -0500

    add .env

--- app.py
+++ app.py
@@ -4,10 +4,19 @@ import uvicorn
 from pymongo import MongoClient
 from fastapi.openapi.utils import get_openapi
 from bson import json_util
+from dotenv import load_dotenv
+import os
 
 app = FastAPI()
 
-client = MongoClient(host="192.168.5.92", port=27017)#172.17.0.2
+load_dotenv()
+
+HOST_MongoDB = os.getenv('HOST_MongoDB')
+PORT_MongoDB = os.getenv('PORT_MongoDB')
+
+print(PORT_MongoDB)
+
+client = MongoClient(host=HOST_MongoDB, port=int(PORT_MongoDB))
 
 db = client["ejercicio_db"]
 collection = db["personal_information"]
